e02c37d15ccdc245740ad2d60408d14a8847d922
VitalieLazari <37633761+VitalieLazari@users.noreply.github.com>
2018-04-05 08:09:32 +0200

    Update Problem2.java

--- src/com/sckeedoo/certification/Problem2.java
+++ src/com/sckeedoo/certification/Problem2.java
@@ -11,12 +11,18 @@ public class Problem2 {
 
     public static void main(String[] args) {
         Player[] players = new Player[5];
-
+        int maxPoints = 0;
+        int numberOfWinners = 0;
+        //create a Player object, we will use it to save results
+        //Player winner = new Player();
+        
+        
         // Adding John
         players[0] = new Player();
         players[0].name = "John";
         players[0].age = 17;
-        players[0].points = 200;
+        //players[0].points = 200;
+        players[0].points = 290;
 
         // Adding Steven
         players[1] = new Player();
@@ -28,7 +34,7 @@ public class Problem2 {
         players[2] = new Player();
         players[2].name = "Maria";
         players[2].age = 16;
-        players[2].points = 250;
+        players[2].points = 270;
 
         // Adding Anna
         players[3] = new Player();
@@ -40,10 +46,44 @@ public class Problem2 {
         players[4] = new Player();
         players[4].name = "Igor";
         players[4].age = 19;
-        players[4].points = 270;
+        players[4].points = 290;
 
         /**
          * Print the winner's name and age. More points the better.
          */
+         //going through array using foreach to find the best result
+         //there can be more than 1, so we are looking for this condition too
+        for (Player player:players)
+        {
+            if (player.points > maxPoints){
+            maxPoints = player.points;
+            numberOfWinners = 1;
+            }
+            else if (player.points == maxPoints){
+             numberOfWinners++;
+            }
+        }
+        
+        //create new array to populate with winner(s), array lenght is equal to numberOfWinners
+        Player[] winners = new Player[numberOfWinners];
+        
+        //reset numberOfWinners, don't need it anymore for previous purpose
+        numberOfWinners = 0;
+        
+        //going through array "player" to extarct winners and populate array "winners"
+        for (Player player:players)
+        {
+            if (player.points == maxPoints){
+            winners[numberOfWinners] = new Player();
+            winners[numberOfWinners] = player;
+            numberOfWinners++;
+            }
+        }
+        
+        //Printing results
+        for (Player winner:winners)
+        {
+            System.out.println("The winner is "+winner.name+", " + winner.age + " years old, having "+ winner.points+" points.");
+        }
     }
 }
